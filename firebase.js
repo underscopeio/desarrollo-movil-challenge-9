05b7db6318c14087aa40694abe0f9adf696ab9a8
Carlos Castro <charly@MacBook-Pro-de-Carlos.local>
2018-10-20 11:00:21 -0300

    Parte 1 v2

---
+++ firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase"
+// Initialize Firebase
+const config = {
+    apiKey: "AIzaSyCj2CFgcaf6mZDym9nhhg_oorF9Ru6PXK4",
+    authDomain: "appchallenge9.firebaseapp.com",
+    databaseURL: "https://appchallenge9.firebaseio.com",
+    projectId: "appchallenge9",
+    storageBucket: "appchallenge9.appspot.com",
+    messagingSenderId: "248849209678"
+  };
+  firebase.initializeApp(config);
+
